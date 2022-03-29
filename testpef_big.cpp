83dd018d7f1c7f9d29ed1191ab7aac8ce02323fb
MrYhatoh <gabriel.carmonat@sansano.usm.cl>
2022-03-29 02:23:02 -0300

    test big

--- testpef_big.cpp
+++ testpef_big.cpp
@@ -1,18 +1,34 @@
 #include "pef_vector_unif.hpp"
 #include "pef_vector_opt.hpp"
+#include "fstream"
+#include "iostream"
 
 #define N 196433254820
 
 int main() {
   std::vector<uint64_t> pb;
+  bit_vector b(N);
   uint64_t pos;
-  while(cin >> pos){
-    pb.push_back(pos);
+
+  ofstream rf("/data/bitvectors/ii/gov2/url/gov2_ii_nofreq_url_dif.txt.dat.100000", std::ios::binary);
+
+  if(!rf){
+    std::cout << "Cannot open file!\n";
+    return 1;
   }
-
-  cout << "-\n";
+  
+  while(!rf.eof()){
+    rf.read((char*) &pos, sizeof(uint64_t));
+    pb.push_back(pos);
+    b[pos] = 1;
+  }
+  rf.close();
+  
+  sd_vector<> sd(b);
+  cout << "Size PEF SD_VECTOR " << size_in_bytes(sd) << "\n"; 
   pef_vector_unif<1024, rank_support_v5<1>, select_support_mcl<1>> pef_unif(pb, N);
-  cout << "Size PEF UNIF BIT VECTOR " << pef_unif.size_in_bytes() << "\n";
-
+  cout << "Size PEF UNIF BIT VECTOR POS ONES " << pef_unif.size_in_bytes() << "\n";
+  pef_vector_opt<rank_support_v5<1>, select_support_mcl<1>> pef_opt(pb, N);
+  cout << "Size PEF OPT BIT VECTOR POS ONES " << pef_opt.size_in_bytes() << "\n";
   return 0;
 }
